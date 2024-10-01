d116eb393e3a253c884951b9532e622bb2a3133e
Aleksandr Viznyi <senak98@yandex.ru>
2024-10-01 19:17:27 +0300

    add reates

--- src/routes/order.ts
+++ src/routes/order.ts
@@ -1,22 +1,29 @@
-import { Router } from 'express';
-import Order from '../models/Order';
+import { Router, Request, Response } from "express";
+import Order from "../models/Order";
 import { v4 as uuidv4 } from 'uuid';
+// import { nanoid } from "nanoid";
 
 const router = Router();
 
-router.post('/', async (req, res) => {
+router.post("/", async (req: Request, res: Response) => {
   const { amountGive, currencyGive, amountReceive, currencyReceive } = req.body;
 
-  if (
-    !amountGive ||
-    !currencyGive ||
-    !amountReceive ||
-    !currencyReceive
-  ) {
-    return res.status(400).json({ message: 'Invalid order data' });
+  if (!amountGive || !currencyGive || !amountReceive || !currencyReceive) {
+    return res.status(400).json({ message: "Invalid order data" });
   }
 
-  const orderId = uuidv4(); // Генерируем уникальный идентификатор заказа
+  let orderId: string;
+  let isUnique = false;
+
+  orderId = ""
+  // Попытка генерации уникального orderId
+  while (!isUnique) {
+    orderId = uuidv4().split('-')[0]; // Берем первую часть UUID для короткого ID
+    const existingOrder = await Order.findOne({ orderId });
+    if (!existingOrder) {
+      isUnique = true;
+    }
+  }
 
   const order = new Order({
     orderId,
@@ -28,9 +35,9 @@ router.post('/', async (req, res) => {
 
   try {
     await order.save();
-    res.json({ message: 'Order created', orderId });
+    res.json({ message: "Order created", orderId });
   } catch (error) {
-    res.status(500).json({ error: 'Server error' });
+    res.status(500).json({ error: "Server error" });
   }
 });
 
--- src/services/exchangeRateService.ts
+++ src/services/exchangeRateService.ts
@@ -1,15 +1,16 @@
-import axios from 'axios';
-import ExchangeRate, { IExchangeRate } from '../models/ExchangeRate';
+import axios from "axios";
+import ExchangeRate, { IExchangeRate } from "../models/ExchangeRate";
 
 const API_KEY = process.env.API_KEY;
 
 const symbolMap: { [key: string]: string } = {
-  'Sber': 'RUB',
-  'T-Bank': 'RUB',
+  Sber: "RUB",
+  "T-Bank": "RUB",
+  
   // другие соответствия, если необходимо
 };
 
-const currencies = ['BTC', 'ETH', 'USDT', 'RUB']; // Список валют, которые вы хотите получить
+const currencies = ['BTC', 'ETH', 'USDT', 'LTC', 'TON', 'RUB']; // Список валют, которые хотиm получить
 
 export const fetchAndSaveExchangeRates = async () => {
   try {
@@ -18,11 +19,11 @@ export const fetchAndSaveExchangeRates = async () => {
     for (const currency of currencies) {
       const symbol = symbolMap[currency] || currency;
       const response = await axios.get(
-        'https://min-api.cryptocompare.com/data/price',
+        "https://min-api.cryptocompare.com/data/price",
         {
           params: {
             fsym: symbol,
-            tsyms: currencies.join(','),
+            tsyms: currencies.join(","),
           },
           headers: {
             authorization: `Apikey ${API_KEY}`,
@@ -35,18 +36,19 @@ export const fetchAndSaveExchangeRates = async () => {
 
     const exchangeRate = new ExchangeRate({ rates });
     await exchangeRate.save();
-    console.log('Exchange rates updated:', rates);
+    console.log("Exchange rates updated:", rates);
   } catch (error) {
-    console.error('Error fetching exchange rates:', error);
+    console.error("Error fetching exchange rates:", error);
   }
 };
 
-export const getLatestExchangeRates = async (): Promise<IExchangeRate | null> => {
-  try {
-    const latestRate = await ExchangeRate.findOne().sort({ timestamp: -1 });
-    return latestRate;
-  } catch (error) {
-    console.error('Error fetching latest exchange rates:', error);
-    return null;
-  }
-};
+export const getLatestExchangeRates =
+  async (): Promise<IExchangeRate | null> => {
+    try {
+      const latestRate = await ExchangeRate.findOne().sort({ timestamp: -1 });
+      return latestRate;
+    } catch (error) {
+      console.error("Error fetching latest exchange rates:", error);
+      return null;
+    }
+  };
