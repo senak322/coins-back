5ecaffdc96d886507b2929078330a9a8aa5f46bc
Aleksandr Viznyi <senak98@yandex.ru>
2024-10-06 01:45:31 +0300

    fix db

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI || '', {
+    await mongoose.connect(process.env.MONGODB_URI || '', {
       // Опции подключения, если необходимо
     });
     console.log('MongoDB connected');
