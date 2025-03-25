4acd28a72394ad8f27d131a2d6907aa8c573df67
Aleksandr Viznyi <senak98@yandex.ru>
2025-03-25 22:03:05 +0300

    edit: rates

--- src/services/exchangeRateService.ts
+++ src/services/exchangeRateService.ts
@@ -1,60 +1,16 @@
 import axios from "axios";
-import jwt from "jsonwebtoken";
-import crypto from "crypto";
 import ExchangeRate, { IExchangeRate } from "../models/ExchangeRate";
 import dotenv from 'dotenv';
-import { Buffer } from 'node:buffer';
 dotenv.config();
 
-const host = "garantex.org";
-const privateKeyBase64 = process.env.GARANTEX_PRIVATE_KEY || "";
-const uid = process.env.GARANTEX_UID || "";
 
-async function getToken(): Promise<string | null> {
-  const somedata = Buffer.from(privateKeyBase64, "base64").toString("ascii");
 
-  try {
-    let { data } = await axios.post(
-      "https://dauth." + host + "/api/v1/sessions/generate_jwt",
-      {
-        kid: uid,
-        jwt_token: jwt.sign(
-          {
-            exp: Math.round(Date.now() / 1000) + 30 * 60, // JWT Request TTL: 30 minutes
-            jti: crypto.randomBytes(12).toString("hex"),
-          },
-          somedata,
-          { algorithm: "RS256" }
-        ),
-      }
-    );
-    return data.token;
-  } catch (e) {
-    console.error(e);
-    return null;
-  }
-}
-
-async function getUsdtToRubRate(token: string): Promise<number> {
-  const { data } = await axios.get(`https://${host}/api/v2/depth?market=usdtrub`, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
+async function getUsdtToRubRate(): Promise<any> {
+  const {data} = await axios.get("https://api.rapira.net/open/market/rates")
+  const price = data.data.find((el: any) => el.symbol === "USDT/RUB");
   
 
-  // Убедитесь, что data содержит нужную структуру
-  // if (!Array.isArray(data) || data.length === 0) {
-  //   throw new Error("Ответ от API не содержит данные или массив пуст");
-  // }
-
-  // Получаем первый элемент и извлекаем его цену
-  const firstItem = data.asks[0]; // Убедитесь, что data[0] существует
-  if (!firstItem || !firstItem.price) {
-    throw new Error("Первый элемент данных не содержит цену");
-  }
-
-  return parseFloat(firstItem.price);
+  return parseFloat(price.bidPrice);
 }
 
 const currencies = [
@@ -101,14 +57,7 @@ export const fetchAndSaveExchangeRates = async () => {
     );
     const rawRates = response.data;
 
-    // Получаем токен Garantex
-    const token = await getToken();
-    if (!token) {
-      throw new Error("Не удалось получить токен для Garantex");
-    }
-
-    // Получаем курс USDT→RUB от Garantex
-    const usdtToRubRate = await getUsdtToRubRate(token);
+    const usdtToRubRate = await getUsdtToRubRate();
 
     const rates: { [key: string]: { rub: number; usd: number } } = {};
 
